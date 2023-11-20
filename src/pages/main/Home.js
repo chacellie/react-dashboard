914f34e24fb403cc6933a9acd94084241090690c
JongwanChoi <jwchoi@kaltour.com>
2023-11-20 18:00:50 +0900

    오류 수정

--- src/pages/main/Home.js
+++ src/pages/main/Home.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
 
 
@@ -12,7 +14,7 @@ const Home = () => {
                     <h1 className="display-4 fst-italic">첫번째 게시글 입니다.</h1>
                     <p className="lead my-3">아직 무엇을 주제로 블로그를 진행할 지 모르겠네요 엘리 아범으로 시작할걸 괜히 체이셀리(Chace + Ellie)로 바꾼걸까요 후후.. 일단! Bootstrap + React + Github 을 사용한 웹 블로그 입니다.</p>
                     <p className="lead mb-0">
-                        <a href="#" className="text-white fw-bold">어디로 연결 시킬까요...?</a>
+                        <Link to="https://naver.com" className="text-white fw-bold">어디로 연결 시킬까요...?</Link>
                     </p>
                 </div>
             </div>
