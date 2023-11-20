ca64e68f999191ee4cb6f25c67b71e809457ed46
JongwanChoi <jwchoi@kaltour.com>
2023-11-20 17:16:23 +0900

    네비게이션 메뉴 적용

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -2,19 +2,7 @@
 const About = () => {
     return (
         <>
-            <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                <h1 className="h2">Welcome CHACELLIE's BLOG</h1>
-            </div>
-
-            <div className="p-4 p-md-5 mb-4 text-white rounded bg-dark">
-                <div className="col-md-7 px-0">
-                    <h1 className="display-4 fst-italic">첫번째 게시글 입니다.</h1>
-                    <p className="lead my-3">아직 무엇을 주제로 블로그를 진행할 지 모르겠네요 엘리 아범으로 시작할걸 괜히 체이셀리(Chace + Ellie)로 바꾼걸까요 후후.. 일단! Bootstrap + React + Github 을 사용한 웹 블로그 입니다.</p>
-                    <p className="lead mb-0">
-                        <a href="#" className="text-white fw-bold">어디로 연결 시킬까요...?</a>
-                    </p>
-                </div>
-            </div>
+           
         </>
     )
 }
--- src/pages/main/Nav.js
+++ src/pages/main/Nav.js
@@ -1,57 +1,36 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FaHome } from 'react-icons/fa';
 import { FaQuestionCircle } from "react-icons/fa";
 const Nav = () => {
 
     return (
-        
-                <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-                    <div className="position-sticky pt-3">
-                        <ul className="nav flex-column">
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link" aria-current="page">
-                                <FaHome className="feather"/>
-                                    Dashboard
-                            </Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <Link to="/about" className="nav-link">
-
-                                <FaQuestionCircle className="feather"/>
-                                    About
-                                </Link>
-                            </li>
-
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    <span data-feather="shopping-cart"></span>
-                                    Products
-                                </a>
-                            </li>
-
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    <span data-feather="users"></span>
-                                    Customers
-                                </a>
-                            </li>
-
-                        </ul>
-
-                        <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                            <span>Saved Report</span>
-                            <a className="link-secondary" href="#">
-                                <span data-feather="plus-circle"></span>
-                            </a>
-
-                        </h6>
-
+        <nav id="sidebarMenu" className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+            <NavLink to="/" className="nav-link" activeClassName="active" aria-current="page">
+                <FaHome className="feather" />
+                Home
+            </NavLink>
+            <div className="accordion" id="accordion">
+                <div className="accordion-item">
+                    <span className="accordion-header" id="headingOne" >
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            프론트엔드
+                        </button>
+                    </span>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordion">
+                        <div class="accordion-body">
+                            <ul className="nav flex-column">
+                                <li className="nav-item">
+                                    <NavLink to="/about" className="nav-link" activeClassName="active" >
+                                        <FaQuestionCircle className="feather" />
+                                        About
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-
-
-                </nav>
-
+                </div>
+            </div>
+        </nav>
     )
 
 }
